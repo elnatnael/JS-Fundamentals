3ac62508d7d00890566e179d7d725fdecf5a3373
elnatnael <meranat9104@gmail.com>
2025-09-14 12:39:42 +0300

    An Integer

---
+++ 5-to_integer.js
@@ -0,0 +1,17 @@
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+    console.log("Usage: node script.js <number>");
+    process.exit(1);
+}
+
+const inputNumber = parseInt(args[0]);
+
+if (isNaN(inputNumber)) {
+    console.log("Not a number");
+    process.exit(1);
+}
+
+console.log("Original string:", args[0]);
+console.log("My number: ", inputNumber);
+
