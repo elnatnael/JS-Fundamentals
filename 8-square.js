81908ce9acef0de2cf7bbde52542ab37551d4108
elnatnael <meranat9104@gmail.com>
2025-09-14 14:52:13 +0300

    squaring

--- 8-square.js
+++ 8-square.js
@@ -12,7 +12,7 @@ if (size <= 0) {
 for (let i = 0; i < size; i++) {
     let row = '';
     for (let j = 0; j < size; j++) {
-        row += 'x';
+        row += 'X';
     }
     console.log(row);
 }
