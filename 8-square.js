2c791763e2481a648b79bafa5f0eb883a1a8fd01
elnatnael <meranat9104@gmail.com>
2025-09-14 14:50:20 +0300

    squaring

---
+++ 8-square.js
@@ -0,0 +1,18 @@
+const size  = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+    console.log('Missing size');
+    process.exit(1);
+}
+
+if (size <= 0) {
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+        row += 'x';
+    }
+    console.log(row);
+}
